3c2764a0c0d479eb3bd134e8c988b272a1f3ee40
heather knight and sailor winkelman <heather@>
2016-09-28 20:03:30 -0700

    Edit js to make each entry uppercase using less repetitive code

--- js/script.js
+++ js/script.js
@@ -2,19 +2,26 @@ $(document).ready(function() {
   $("form#list").submit(function() {
     event.preventDefault();
 
-    var grocery1 = $("input#entry1").val();
+    /*var grocery1 = $("input#entry1").val();
     var grocery2 = $("input#entry2").val();
     var grocery3 = $("input#entry3").val();
     var grocery4 = $("input#entry4").val();
-    var grocery5 = $("input#entry5").val();
+    var grocery5 = $("input#entry5").val();*/
 
-    var groceries = [grocery1, grocery2, grocery3, grocery4, grocery5];
+    var groceries = ["entry1", "entry2", "entry3", "entry4", "entry5"];
+    var fancyGross = [];
 
-    var upperGroceries = groceries.map(function(grocery) {
-      return grocery.toUpperCase();
+
+   groceries.forEach(function(grocery) {
+      var userInput = $("input#" + grocery).val();
+      fancyGross.push(userInput);
     });
 
+    var finalGross = fancyGross.map(function(gross) {
+        return gross.toUpperCase();
+      });
+
     $("#list").hide();
-    $("#output").text(upperGroceries);
+    $("#output").text(finalGross);
   });
 });
