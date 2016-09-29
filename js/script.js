77418348eb7546463beaa0cdacc3c638d3b22479
heather knight and sailor winkelman <heather@>
2016-09-28 20:20:31 -0700

    Add js functions to make the list sort in alphabetical order in an unordered list.

--- js/script.js
+++ js/script.js
@@ -2,26 +2,25 @@ $(document).ready(function() {
   $("form#list").submit(function() {
     event.preventDefault();
 
-    /*var grocery1 = $("input#entry1").val();
-    var grocery2 = $("input#entry2").val();
-    var grocery3 = $("input#entry3").val();
-    var grocery4 = $("input#entry4").val();
-    var grocery5 = $("input#entry5").val();*/
-
     var groceries = ["entry1", "entry2", "entry3", "entry4", "entry5"];
     var fancyGross = [];
-
 
    groceries.forEach(function(grocery) {
       var userInput = $("input#" + grocery).val();
       fancyGross.push(userInput);
     });
-
-    var finalGross = fancyGross.map(function(gross) {
+    
+    var upperGross = fancyGross.map(function(gross) {
         return gross.toUpperCase();
       });
 
-    $("#list").hide();
-    $("#output").text(finalGross);
+    upperGross.sort();
+
+    $("#list").fadeOut();
+    $("#output").slideDown();
+
+    upperGross.forEach(function(list) {
+      $("#output").append("<li>" + list + "</li>");
+    });
   });
 });
